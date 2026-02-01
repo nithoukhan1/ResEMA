f7943419ea3c6d01c88229241cbbb239f6476cde
Inayat Ullah Khan <88286214+nithoukhan1@users.noreply.github.com>
2026-02-01 09:04:45 +0000

    block.py updated

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -2065,3 +2065,18 @@ class RealNVP(nn.Module):
             self.float()
         z, log_det = self.backward_p(x)
         return self.prior.log_prob(z) + log_det
+    
+    
+class SPD(nn.Module):
+    # Space-to-Depth layer for lossless downsampling
+    def __init__(self, dimension=1):
+        super().__init__()
+        self.d = dimension
+
+    def forward(self, x):
+        return torch.cat([
+            x[..., ::2, ::2],
+            x[..., 1::2, ::2],
+            x[..., ::2, 1::2],
+            x[..., 1::2, 1::2]
+        ], 1)
